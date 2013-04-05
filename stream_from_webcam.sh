bff4c0cacb0ce5a217e951c12d0ac8fb9275862a
Robby Ranshous <rranshous@gmail.com>
2013-04-05 17:07:08 -0400

    h256 in mp4ts

--- stream_from_webcam.sh
+++ stream_from_webcam.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
 
-./bin/ffmpeg -f video4linux2 -s 1280x720 -i /dev/video0 -vf scale=400:-1 -preset ultrafast -r 30 -f mpegts - | nc $1 8000
+./bin/ffmpeg -f video4linux2 -s 1280x720 -i /dev/video0 -vf scale=400:-1 -preset ultrafast -r 30 -vbsf h264_mp4toannexb -vcodec h264 -f mpegts - | nc $1 8000
