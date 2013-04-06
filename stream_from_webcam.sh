3b34df2c9eb17ecb69cf27034e872d8c1b55d734
Robby Ranshous <rranshous@gmail.com>
2013-04-05 20:19:24 -0400

    math to fix even heights issue

--- stream_from_webcam.sh
+++ stream_from_webcam.sh
@@ -1,5 +1,5 @@
 
 #!/bin/bash
 
-./bin/ffmpeg -r 15 -vcodec mjpeg -f video4linux2 -s 960x544 -i /dev/video0  -tune zerolatency -preset ultrafast -r 30 -vbsf h264_mp4toannexb -vcodec h264 -f mpegts - | nc $1 8000
-#./bin/ffmpeg -r 15 -vcodec mjpeg -f video4linux2 -s 960x544 -i /dev/video0 -vf scale=400:-1 -tune zerolatency -preset ultrafast -r 30 -vbsf h264_mp4toannexb -vcodec h264 -f mpegts - | nc $1 8000
+# crazy scale is to make sure the height is divisible by 2
+./bin/ffmpeg -r 15 -vcodec mjpeg -f video4linux2 -s 960x544 -i /dev/video0 -vf scale=400:trunc\(ow\/a\/2\)*2 -tune zerolatency -preset ultrafast -r 30 -vbsf h264_mp4toannexb -vcodec h264 -y -re -f mpegts - | nc $1 8000
