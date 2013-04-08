447edc76cdbf3d0137bac432fc1847550beef65b
Robby Ranshous <rranshous@gmail.com>
2013-04-08 17:56:10 -0400

    updated stream video script to use newest improvements

--- stream_video.sh
+++ stream_video.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./bin/ffmpeg -i $1 -vf scale=400:-1 -preset ultrafast -vbsf h264_mp4toannexb -vcodec h264 -f mpegts - | nc $2 8000
+./bin/ffmpeg -i $1 -vf scale=400:trunc\(ow\/a\/2\)*2 -tune zerolatency -preset ultrafast -r 30 -vbsf h264_mp4toannexb -vcodec h264 -y -re -f mpegts - | nc $2 8000
